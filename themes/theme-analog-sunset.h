79a757d11660b9645426555b48255de38d8e84eb
atdeters <sreted@outlook.com>
2025-12-21 01:53:09 +0100

    theme: Add gpt generated themes

---
+++ themes/theme-analog-sunset.h
@@ -0,0 +1,24 @@
+#ifndef THEME_ANALOG_SUNSET_H
+# define THEME_ANALOG_SUNSET_H
+
+# define COL1  0xFFF3E6   // Warm Cream
+# define COL2  0xFFE0C7   // Peach Fade
+# define COL3  0xF6C1A1   // Dusty Apricot
+# define COL4  0xE9A178   // Sunset Orange
+# define COL5  0xD9825B   // Burnt Peach
+# define COL6  0xC46A4A   // Terracotta
+# define COL7  0xAD5645   // Clay Red
+# define COL8  0x95403F   // Brick Rose
+# define COL9  0x7E3342   // Wine Shadow
+# define COL10 0x6A2C47   // Dusk Purple
+# define COL11 0x5A274A   // Plum Ink
+# define COL12 0x4A1F3F   // Film Shadow
+# define COL13 0xE0B084   // Faded Gold
+# define COL14 0xC9946A   // Sun Dust
+# define COL15 0x8A3A3A   // Old Red
+# define COL16 0x3B1F1F   // Dark Film
+
+# define COL_BACK 0xF2E6D8   // Aged Paper
+# define COL_GRID 0xC46A4A   // Sunset Grid
+
+#endif
---
+++ themes/theme-monochrome-plus.h
@@ -0,0 +1,24 @@
+#ifndef THEME_MONOCHROME_PLUS_H
+# define THEME_MONOCHROME_PLUS_H
+
+# define COL1  0xF2F2F2   // Near White
+# define COL2  0xE0E0E0   // Light Gray
+# define COL3  0xCFCFCF   // Soft Gray
+# define COL4  0xBDBDBD   // Mid Gray
+# define COL5  0xA8A8A8   // Neutral Gray
+# define COL6  0x8F8F8F   // Dark Gray
+# define COL7  0x7A7A7A   // Charcoal
+# define COL8  0x666666   // Deep Gray
+# define COL9  0x4F4F4F   // Graphite
+# define COL10 0x3A3A3A   // Almost Black
+# define COL11 0x00C8FF   // Accent Cyan
+# define COL12 0x00A4D6   // Deep Cyan
+# define COL13 0x007EA8   // Steel Cyan
+# define COL14 0x005F80   // Dark Accent
+# define COL15 0x003F59   // Near Black Cyan
+# define COL16 0x1A1A1A   // Black Core
+
+# define COL_BACK 0xFFFFFF   // White Background
+# define COL_GRID 0xD0D0D0   // Light Grid
+
+#endif
